89f267d7c991684d267e02b2cef0d1e7666a82ce
eduard-ops <epustynnik@gmail.com>
2022-07-29 21:36:26 +0300

    add services

---
+++ src/services/currency-api.js
@@ -0,0 +1,41 @@
+const BAZE_URL = 'https://api.apilayer.com';
+
+const API_KEY = 'EjSq0HOafXUWfGgQXFRoXkBxCns4FP8V';
+
+const myHeaders = new Headers();
+myHeaders.append('apikey', API_KEY);
+
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders,
+};
+
+async function mainFetchApi(url = '', config = {}) {
+  const response = await fetch(url, config);
+  return response.ok
+    ? await response.json()
+    : Promise.reject(new Error('Not found'));
+}
+
+export function fetchCurrency() {
+  return mainFetchApi(`${BAZE_URL}/exchangerates_data/latest`, requestOptions);
+}
+
+export function fetchCurrencyHeader() {
+  return mainFetchApi(
+    `${BAZE_URL}/exchangerates_data/latest?symbol=USD,EUR,UAH`,
+    requestOptions
+  );
+}
+
+// export function roman(params) {
+//   return mainFetchApi(
+//     `${BAZE_URL}/exchangerates_data/latest?symbols=USD,EUR,UAH`,
+//     requestOptions
+//   );
+// }
+
+// export function fetchRandomJoke() {
+//   return mainFetchApi(`${BAZE_URL}/jokes/random`);
+// }
